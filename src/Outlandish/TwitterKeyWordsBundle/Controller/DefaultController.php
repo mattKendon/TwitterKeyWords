b0ed7d33905a82b4e53a13275f83b47f95100b6e
Matthew Kendon <mkendon@gmail.com>
2014-12-09 01:17:43 +0000

    some shifting around of things

--- src/Outlandish/TwitterKeyWordsBundle/Controller/DefaultController.php
+++ src/Outlandish/TwitterKeyWordsBundle/Controller/DefaultController.php
@@ -8,6 +8,11 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        if(!$name) {
+
+            return $this->render('OutlandishTwitterKeyWordsBundle:Default:noName.html.twig');
+
+        }
         return $this->render('OutlandishTwitterKeyWordsBundle:Default:index.html.twig', array('name' => $name));
     }
 }
